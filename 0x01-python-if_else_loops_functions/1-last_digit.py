2c66a64bc62a29cba94b6e7f64bb4ab2b4b427cf
Kevin Koech <kevinkoech@proton.me>
2023-05-12 00:43:12 +0300

    minor change for negative num

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_dig = number % 10
+if number >= 0:
+    last_dig = number % 10
+else:
+    last_dig = number % -10
+
 if last_dig > 5:
     print(f"Last digit of {number} is {last_dig} and is greater than 5")
 elif last_dig == 0:
