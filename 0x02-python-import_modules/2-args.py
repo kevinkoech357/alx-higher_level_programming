5f18f64e41bcf9b57e15e4a93e7e05a3d1f58f69
Kevin Koech <kevinkoech@proton.me>
2023-05-11 22:03:40 +0300

    prints the number of and list of arguments passed when run

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    length = len(sys.argv) - 1
+    if length == 0:
+        print("0 arguments.")
+    elif length == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(length))
+    for index in range(length):
+        print("{}: {}".format(index + 1, sys.argv[index + 1]))
