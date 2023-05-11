a7102a7a4b6c112fc31fe3a21822d5373bbaded8
Kevin Koech <kevinkoech@proton.me>
2023-05-12 01:03:29 +0300

    correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
 def uppercase(str):
-    transform = ""
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
-            transform += chr(ord(c) - 32)
-        else:
-            transform += c
-    print(transform + "\n")
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
