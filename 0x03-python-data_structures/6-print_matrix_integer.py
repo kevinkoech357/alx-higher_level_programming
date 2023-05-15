e2f9304f1af29045a57e179db468405a64b30580
Kevin Koech <kevinkoech@proton.me>
2023-05-15 16:36:33 +0300

    a function that prints integers in a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+    for row in matrix:
+        for column in row:
+            if column != row[-1]:
+                print("{:d}".format(column), end=" ")
+            else:
+                print("{:d}".format(column), end="")
+        print()
