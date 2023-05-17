c5f39cd2a6bd90ba474011f9640e2a2796bbdbaa
Kevin Koech <kevinkoech@proton.me>
2023-05-17 22:02:30 +0300

    correction in print func to use str.format

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@
 def print_sorted_dictionary(a_dictionary):
     after_sort = sorted(a_dictionary.keys())
     for key in after_sort:
-        print(key, ':', a_dictionary[key])
+        print("{}: {}".format(key, a_dictionary[key]))
