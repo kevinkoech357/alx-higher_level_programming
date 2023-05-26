8c4743530483eb4a1b09b9e7e3239cdb6de82e77
Kevin Koech <kevinkoech@proton.me>
2023-05-26 12:05:40 +0300

    boilerplate

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* is_palindrome - checks if listint_t is palindrome
+*
+* @head: pointer to pointer to head
+*
+* Return: 0 if successful
+*
+*/
+
+int is_palindrome(listint_t **head)
+{
+
+}
