92a3c4630656b253a39ba523c96e87298754d02c
Kevin Koech <kevinkoech@proton.me>
2023-05-30 15:51:32 +0300

    initialized with size set to private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+To be updated in later programs of this class.
+"""
+
+
+class Square:
+    """
+    Initialize a private instance size.
+    """
+    def __init__(self, size):
+        self.__size = size
