ebd3010135e738eb6041b18082ca78cd5678a6ff
Kevin Koech <kevinkoech@proton.me>
2023-06-12 22:46:34 +0300

    Raise an Exception if area is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This module creates a class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A class called BaseGeometry.
+    """
+    def area(self):
+        """
+        Raises an Exception.
+        """
+        raise Exception("area() is not implemented")
