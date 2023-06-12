9fae0014fd25a9b3b0f2263cf9cec5cdc00191cc
Kevin Koech <kevinkoech@proton.me>
2023-06-12 22:47:41 +0300

    Validate value by checking if it's an integer and > 0

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+This module creates a class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A class called BaseGeometry.
+    """
+    def area(self):
+        """
+        Raises an Exception.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates value.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
