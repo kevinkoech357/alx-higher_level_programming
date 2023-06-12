e8556edeedfac7038e401316a020fa5d63ea07bc
Kevin Koech <kevinkoech@proton.me>
2023-06-12 23:38:32 +0300

    Correction

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-"""
-Creates a new subclass that inherits
-from Rectangle.
-"""
-
-
+# importing
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 Rectangle = __import__("9-rectangle").Rectangle
+
+"""
+This module reates a new subclass that inherits
+from Rectangle.
+"""
 
 
 class Square(Rectangle):
