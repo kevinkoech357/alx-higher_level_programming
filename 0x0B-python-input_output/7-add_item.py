94e9bf200b3f0be373f2c4429dff8624a48c191a
Kevin Koech <kevinkoech@proton.me>
2023-06-14 13:41:44 +0300

    pycodestyle correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,15 @@ and then save them to a file
 """
 
 
-#importing
+# importing
 import os
 import sys
 
 
 if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    load_from_json_file = __import__(
+        "6-load_from_json_file").load_from_json_file
 
     my_list = []
 
