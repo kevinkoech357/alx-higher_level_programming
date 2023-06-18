2a0ffddd945a75aa9c5741546f19cebaa9d54075
Kevin Koech <kevinkoech@proton.me>
2023-06-18 21:30:42 +0300

    create a class named Base which will be the parent class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Model for a class named Base.
+"""
+
+
+class Base:
+    """
+    A class named Base.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Instantiating Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
