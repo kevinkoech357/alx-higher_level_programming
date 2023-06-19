0738a7f1392df7522dd6e6e2e5ff97dd9d52cab4
Kevin Koech <kevinkoech@proton.me>
2023-06-19 21:00:10 +0300

    add getter and setter methods for x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,49 @@ class Rectangle(Base):
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    @property
+    def x(self):
+        """
+        Retrieves x.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Sets and validates x.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        """
+        Retrieves y.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Sets and validates y.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
