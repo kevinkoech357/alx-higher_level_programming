b50b90a1904890565f818857257cba3341e6a68c
Kevin Koech <kevinkoech@proton.me>
2023-06-22 21:04:10 +0300

    use arg and kwargs to assign arguments to attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,6 +46,30 @@ class Square(Rectangle):
             self.id, self.x, self.y, self.height
         ))
 
+    def update(self, *args, **kwargs):
+        """
+        Assigns an argument to each attribute.
+        """
+        if len(args) != 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
     def to_dictionary(self):
         """
         Returns dictionary representation
