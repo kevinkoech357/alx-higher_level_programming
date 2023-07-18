7de7b74afbd9b016da307dcb7a549578f96f28f0
Kevin Koech <kevinkoech@proton.me>
2023-07-18 22:30:15 +0300

    convert db, first table to UTF-8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Converts Database hbtn_0c_0,
+-- Table first_table,
+-- Field name in first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
