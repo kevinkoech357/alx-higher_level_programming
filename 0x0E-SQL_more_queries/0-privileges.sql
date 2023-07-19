f031361c0a6bc18cf776598d48ea73668fa1b5d4
Kevin Koech <kevinkoech@proton.me>
2023-07-19 16:31:44 +0300

    correction

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- Lists all priviledges of user_0d_1 and user_0d_2 on the server
 
+CREATE USER user_0d_1@localhost;
+CREATE USER user_0d_2@localhost;
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
