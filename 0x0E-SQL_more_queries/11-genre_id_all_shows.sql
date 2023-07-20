e2a0f8815da9facaf53fb3d95bfb3b4a268d2156
Kevin Koech <kevinkoech@proton.me>
2023-07-20 22:27:11 +0300

    lists all shows contained in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesnt have a genre, display NULL
+-- You can use only one SELECT statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
