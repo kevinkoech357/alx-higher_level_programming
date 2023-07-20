d1eaa644562ff3dfe2a77685d0032c938eaec1f5
Kevin Koech <kevinkoech@proton.me>
2023-07-20 21:01:59 +0300

    correction

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Lists all priviledges of user_0d_1 and user_0d_2 on the server
 -- on the server(in localhost)
 
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 -- If user exists, script will not fail
 -- Assigns a password user_0d_1_pwd
 
-CREATE USER IF NOT EXISTS user_0d_1_pwd@localhost
+CREATE USER IF NOT EXISTS 'user_0d_1_pwd'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
