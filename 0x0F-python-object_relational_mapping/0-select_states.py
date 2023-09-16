ea590aa35ca2bfb63f9d545b630ed38e48542d87
Kevin Koech <kevinkoech@proton.me>
2023-09-16 13:25:38 +0300

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This script lists all states from hbtn_0e_0_usa db
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                               passwd=argv[2], db=argv[3])
+    
+    cursor = db.cursor()
+    query = "SELECT * FROM states ORDER BY states.id"
+    rows = cursor.execute(query)
+
+    for row in range(rows):
+        print(cursor.fetchone())
