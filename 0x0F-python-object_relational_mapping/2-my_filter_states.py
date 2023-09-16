69bfb912e00607c7096c7b21786ced3b7e5022f5
Kevin Koech <kevinkoech@proton.me>
2023-09-16 14:16:57 +0300

    take user input and return if match is found

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Takes user input and displays where name == user_input
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    user_input = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id",
+                (user_input,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
