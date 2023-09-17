cf8240a2f83fc9d19cf60e9ba76c7386d5fdc0c3
Kevin Koech <kevinkoech@proton.me>
2023-09-17 19:08:55 +0300

    use Metadata object

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,14 +6,13 @@ Defines a class State which is an instance
 of Base=declarative_base()
 """
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sys import argv
 
 
-# Crete an instance of declarative_base
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
 
 class State(Base):
     """
@@ -23,15 +22,8 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(
+        Integer, primary_key=True, nullable=False,
+        unique=True, autoincrement=True
+    )
     name = Column(String(255), nullable=False)
-
-# Creating a connection to database
-engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
-
-# Create the tables in the database
-Base.metadata.create_all(engine)
-
-# Create a session
-Session = sessionmaker(bind=engine)
-session = Session()
