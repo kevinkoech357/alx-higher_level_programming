deab3de76b90b31a997388337bd9d79d49250b64
Kevin Koech <kevinkoech@proton.me>
2023-09-28 08:20:20 -0700

    fix shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script uses curl to send a request to a URL provided as arg and displays size of the body of the response
 curl -sI "$1" | grep -iF "Content-Length" | sed s/"Content-Length: "//
