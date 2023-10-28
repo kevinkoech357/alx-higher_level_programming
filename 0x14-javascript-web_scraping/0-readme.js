4003cb73ec5d17d0df44c51a6ec6aa9ef2484338
Kevin_Koech <kevinkoech@proton.me>
2023-10-28 19:17:20 +0300

    read file contents

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if a file path is provided as an argument
+if (process.argv.length !== 3) {
+  console.error('Usage: node read_file.js <file_path>');
+  process.exit(1); // Exit with an error code
+}
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+// Read the file with utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
