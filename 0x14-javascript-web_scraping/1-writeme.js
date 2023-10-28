73d2ac059b30631359cb63c4a0727315c54ef7a3
Kevin_Koech <kevinkoech@proton.me>
2023-10-28 19:21:18 +0300

    write data to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if both file path and content arguments are provided
+if (process.argv.length !== 4) {
+  console.error('Usage: node write_file.js <file_path> "<content>"');
+  process.exit(1); // Exit with an error code
+}
+
+// Get the file path and content from the command line arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Write the content to the file with utf-8 encoding
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Content has been successfully written to the file.');
+  }
+});
