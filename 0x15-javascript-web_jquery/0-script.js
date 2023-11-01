f5ccf32fa82454287604f66786af5436ce727037
Kevin_Koech <kevinkoech@proton.me>
2023-11-01 19:09:31 +0300

    change color of header

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+      // Select the header
+      const header = document.querySelector("header");
+      header.style.color = "red";
+    });
