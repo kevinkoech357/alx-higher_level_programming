72ab44b049f81423f809a9f6eab9fb0756abe06f
Kevin_Koech <kevinkoech@proton.me>
2023-11-01 19:16:49 +0300

    change color to red onclick

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#red_header").click(function() {
+      // Change the text color of the element with id "red_header" to red
+      $(this).css("color", "red");
+    });
+  });
